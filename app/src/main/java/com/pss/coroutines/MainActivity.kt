21489c5493430903346060847778ddddf6d17dfb
ParkSangSun <appleasz1028102@gmail.com>
2022-04-21 17:15:07 +0900

    Coroutine job exception handling

--- app/src/main/java/com/pss/coroutines/MainActivity.kt
+++ app/src/main/java/com/pss/coroutines/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.util.Log
 import androidx.annotation.MainThread
 import kotlinx.coroutines.*
+import java.lang.IllegalArgumentException
 import kotlin.concurrent.thread
 
 class MainActivity : AppCompatActivity() {
@@ -43,17 +44,42 @@ class MainActivity : AppCompatActivity() {
          }
          job.cancel()*/
 
+    /*    CoroutineScope(Dispatchers.Main).launch {
+        }
+        val job = CoroutineScope(Dispatchers.IO).launch(start = CoroutineStart.LAZY) {
+            delay(1000)
+            Log.d("TAG", "코루틴")
+        }
+        job.start()
+        job.cancel("job Cancelled", InterruptedException("Cancelled Forcibly"))
+        Log.d("TAG", "job state : ${job.isCancelled}")
+        job.invokeOnCompletion {
+            Log.d("TAG", "throwable : $it")
+        }
+        Log.d("TAG", "job state : ${job.isCompleted}")
+        Log.d("TAG", job.getCancellationException().toString())
+    */
+
+
+
         CoroutineScope(Dispatchers.Main).launch {
-            val job = CoroutineScope(Dispatchers.IO).launch(start = CoroutineStart.LAZY) {
+            val exceptionHandler = CoroutineExceptionHandler { _, throwable ->
+                Log.d("TAG","exceptionHandler : $throwable")
+                when(throwable){
+                    is IllegalArgumentException -> { }
+                    is InterruptedException -> { }
+                }
+            }
+            val job = CoroutineScope(Dispatchers.IO).launch(
+                start = CoroutineStart.LAZY,
+                context = exceptionHandler
+            ) {
                 delay(1000)
                 Log.d("TAG","코루틴")
+                delay(4000)
+                throw InterruptedException()
             }
-            job.cancel("job Cancelled by SangSun", InterruptedException("Cancelled Forcibly"))
-            job.invokeOnCompletion {
-                Log.d("TAG", "throwable : $it")
-            }
-            Log.d("TAG", job.getCancellationException().toString())
+            job.start()
         }
-
     }
 }
